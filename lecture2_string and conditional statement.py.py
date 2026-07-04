e1b98c0339c87cd3e99f841fd53a24e3d693dbc4
yash katarnaware <yashkatarnwre@gmail.com>
2026-07-04 21:32:25 +0530

    string and conditional statement

---
+++ lecture2_string and conditional statement.py.py
@@ -0,0 +1,98 @@
+#string
+'''str1 = "hey my name is yash.\ni am 19 years old."
+str2 = "hey my name is yash.\ti am 19 years old."
+
+print(str1)
+print(str2)'''
+
+#concantenation
+'''
+str11 = "Yash"
+str22 = "katarnaware"
+final_str = str11 + str22
+
+# print(str11 + str22)
+        #OR
+print(final_str)
+
+print(str11 + " " + str22) #space
+'''
+#length pf str 
+'''
+str3 = "Yash"
+str4 = "Katarnaware"
+final_str = str3 + " " + str4
+
+print(final_str)
+
+len1 = len(str3)
+len2 = len(str4)
+
+ 
+# print(len(str3))
+# print(len(str4))
+     #OR
+print(len1)
+print(len2) 
+print(len(final_str))
+'''
+#indexing character
+
+'''str = "Yash Katarnaware"
+ch = str[6]
+
+print(ch)
+print(str[8])'''
+
+#SLICING
+'''
+str = "Yash Katarnaware"
+print(str[1:4])
+print(str[:4])
+print(str[0:4])
+print(str[4:15])
+print(str[:len(str)])
+print(str[4:16])
+'''
+#negative sclicing
+'''str = "Yash Katarnaware"
+print(str[-10:-1])
+print(str[-11:])'''
+
+
+#string function
+'''
+str = "i am a software engineer"
+
+
+print(str.endswith("er")) #returns True if string end with substr
+print(str.endswith("sof"))  
+
+print(str.capitalize()) # capitalize 1st value
+
+print(str.replace("o","i"))#replace all occurences of old
+print(str.replace("software","hardware"))
+
+print(str.find("e")) #returns 1st index of 1st occurence
+print(str.find("software"))
+
+print(str.count("e"))
+print(str.count("am")) #counts the occurrence of substr
+print(str.count("a"))'''
+
+
+
+#CONDITIONAL STATEMENT
+
+#if-elif-else (syntax)
+'''
+# if(condition):
+#     statement1
+
+# elif(condition):
+#     statement2
+
+# else:
+#     statementN
+'''
+ 
