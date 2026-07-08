dcd35c9da39146f0e79aaeacd38b2f0c576491e1
yash katarnaware <yashkatarnwre@gmail.com>
2026-07-08 18:59:44 +0530

    Create lecture3_list and tupples.py

---
+++ lecture3_list and tupples.py
@@ -0,0 +1,87 @@
+#LIST AND TUPLES
+
+#list iin python
+
+'''marks =[76.5, 78.5, 87.6, 97.67]
+print(marks)
+
+print(len(marks))
+print(type(marks))
+
+student = ["yash", 94.56, "sayale"]
+print(student[0])
+student[0] = "sambodhi" 
+print(student) '''
+
+#List slicing
+
+'''marks = [54, 78, 98, 68, 97, 67]
+
+print(marks[1:5])
+print(marks[:4])
+print(marks[-3:-1])
+print(marks[-6:])'''
+
+#List Method
+
+# List = [2 ,4 ,5 ,7, 3]
+# fruits = ["banana", "chiku", "apple", "mango"]
+'''
+List.append(6) #adds one element at the end
+print(List)
+
+
+List.sort() #sort in ascending order, ascending means a-z order
+print(List)
+fruits.sort()
+print(fruits)
+
+
+List.sort(reverse = True) #sort in descending order , ex . z-a order
+print(List)
+fruits.sort(reverse = True)
+print(fruits)
+
+
+List.reverse()  #reverse list
+fruits.reverse()
+print(List)
+print(fruits)
+'''
+'''
+List = [3, 5, 6]
+List.insert(1, 8) #insert element at index
+print(List)
+'''
+'''list = [3, 4, 6, 9]
+list.remove(6) #remove occurences of an element
+print(list)
+
+list.pop(2) #remove element at index
+print(list)'''
+
+
+#TUPPLES IN PYTHON
+
+'''tup = (4, 5, 7, 3, 6)
+tap = ()
+taap = (1, )#this way to store one tupple  value
+print(tup)
+print(type(tup))
+
+print(tap)
+print(type(tap))'''
+
+#slicing in tupple
+#slicing in tupple is a same like list and string
+'''tup = (4, 6, 7, 3, 8,)
+
+print(tup[1:3])
+print(tup[-4:-1])'''
+
+#Tupple method
+'''tup = ( 2, 4, 5, 3, 5, 5,)
+
+print(tup.index(3)) # return index of 1st occurences
+
+print(tup.count(5)) #counts total occurences '''
