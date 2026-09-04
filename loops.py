78bf3ea0787622ffbe46a028a6ad7867f75b7f4f
yash katarnaware <yashkatarnwre@gmail.com>
2026-09-04 21:57:08 +0530

    ..

---
+++ loops.py
@@ -0,0 +1,29 @@
+#while loop
+
+'''count = 1
+while count<=6 :
+    print("yash!")
+    count += 1
+
+i = 1                # i is a iterator
+while i <= 10 :
+    print("YO!",i)
+    i += 1'''
+
+#print numbers from 1 to 10
+'''
+i = 1
+while i<=10 :
+    print(i)
+    i += 1
+
+print("Loop ended")'''
+
+#print numbers from 10 to 1
+
+y = 10
+while y >= 1 :
+    print(y)
+    y -= 1
+
+print("Loop ended")
